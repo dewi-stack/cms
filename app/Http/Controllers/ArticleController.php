694801ad12ab8ea26db248793dd0a7954eb203a9
= <=>
2022-03-07 20:09:14 +0700

    Kode Program Kedua Revisi

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,4 +10,12 @@ class ArticleController extends Controller
     public function articles($id) {
         return "Ini adalah halaman Artikel dengan ID: ".$id;
     }
+
+    public function about(){
+        return view('/article/about');
+    }
+
+    public function contact(){
+        return view('/article/contact');
+    }
 }
