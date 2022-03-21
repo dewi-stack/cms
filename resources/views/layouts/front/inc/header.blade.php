a4f09fbd22e70a6d3978e0dd5a7720b1e8ddfdc0
= <=>
2022-03-21 15:03:40 +0700

    Kode Program Kelima

--- resources/views/layouts/front/inc/header.blade.php
+++ resources/views/layouts/front/inc/header.blade.php
@@ -15,6 +15,7 @@
             </div>
             <div class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
+                <li><a href="{{url('login')}}"> Login </a></li>
                 <li class="active"><a href="/">Home</a></li>
                 <li><a href="{{url('about')}}">About Us</a></li>
                 <li><a href="{{('contact')}}">Contact</a></li>
